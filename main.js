c9ea20ef4920168a63c4689cca4660073958406d
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-07-27 01:27:59 +0200

    quiz selectio added, quiz functionality updated

--- main.js
+++ main.js
@@ -1,58 +1,120 @@
+let quizTypes = Object.keys(questionData);
+
+
 let currentQuestion = 0;
+let currentQuizName = null;
+let currentQuestions = null;
 
 //If questions just got are initiated -> shuffle them
-shuffleAllQuestions();
 
 //If questions have already been initiated -> overwrite initiated question data with the locally saved data
-loadItems();
-
-
 
 
 function init() {
-    render();
+    loadItems();
+    console.log("diese werte sollten 0 bzw null sein:");
+    console.log(currentQuestion);
+    console.log(currentQuizName);
+    console.log(currentQuestions)
+    if (currentQuestions != null) {
+        setItemInvisible("startCard");
+        setItemInvisible("selectQuizCard");
+        setItemVisible("questionCard");
+        renderQuiz();
+    }
+}
+
+function showQuizSelection() {
+    setItemInvisible("startCard");
+    setItemVisible("selectQuizCard");
+    document.getElementById("quizTypeButtons").innerHTML = getQuizOptionsHtml()
 }
 
 function saveItems() {
-    let questionsText = JSON.stringify(questions);
+    let questionsText = JSON.stringify(currentQuestions);
     localStorage.setItem('questions', questionsText);
 }
+
+
+function selectQuizQuestions(givenQuizId) {
+    currentQuestions=questionData[quizTypes[givenQuizId]];
+    shuffleAllQuestions();
+    init();
+}
+
+
+function getQuizOptionsHtml() {
+    html = "";
+    for (let i = 0; i < quizTypes.length; i++) {
+        html += `<button class='btn btn-start' onclick='selectQuizQuestions(${i})'>${quizTypes[i]}</button>`;
+    }
+    return html;
+}
+
 
 function loadItems() {
     let questionsText = localStorage.getItem('questions');
     if (questionsText) {
-        questions = JSON.parse(questionsText);
+        currentQuestions = JSON.parse(questionsText);
     }
 }
 
 
 function shuffleAllQuestions() {
-    for (let i = 0; i < questions.length; i++) {
-        questions[i]['Answers'] = shuffle(questions[i]['Answers']);
+    for (let i = 0; i < currentQuestions.length; i++) {
+        currentQuestions[i]['Answers'] = shuffle(currentQuestions[i]['Answers']);
     }
 }
 
-function render() {
-    renderQuestion();
-    renderAnswers();
-    renderProgress();
-    saveItems();
+function renderQuiz() {
+    
+    if (checkIfQuizComplete() == false) {
+        renderQuestion();
+        renderAnswers();
+        renderProgress();
+        saveItems(); //checken ob nötig
+    } else {
+        completeQuiz();
+    }
+
+}
+
+function setItemVisible(cardId){
+    document.getElementById(cardId).style = "";
+}
+
+function setItemInvisible(cardId){
+    document.getElementById(cardId).style = "display: none;";
 }
 
 
 function renderQuestion() {
     document.getElementById("question-title").innerHTML = `Frage ${currentQuestion + 1}`;
-    document.getElementById("question-text").innerHTML = questions[currentQuestion]["Question"];
+    document.getElementById("question-text").innerHTML = currentQuestions[currentQuestion]["Question"];
+}
+
+
+function resetQuiz() {
+    currentQuestion = 0;
+    currentQuizName = null;
+    currentQuestions = null;
+    localStorage.removeItem("questions");
+    setItemVisible("startCard");
+    setItemInvisible("selectQuizCard");
+    setItemInvisible("questionCard");
+    setItemInvisible("resultCard");
+    setItemInvisible("trophyImg");
+    init();
 }
 
 
 function renderAnswers() {
-    let answers = (questions[currentQuestion]["Answers"]);
+    let answers = (currentQuestions[currentQuestion]["Answers"]);
     document.getElementById("answers").innerHTML = getAnswersHtml(answers);
 
     //If the User has already given an answer for this question
-    if(questions[currentQuestion]["Selected"]!=null){
-        checkQuestion(questions[currentQuestion]["Selected"]);
+    if (currentQuestions[currentQuestion]["Selected"] != null) {
+        checkQuestion(currentQuestions[currentQuestion]["Selected"]);
 
     }
 }
@@ -60,32 +122,35 @@ function renderAnswers() {
 
 function getAnswersHtml(answers) {
     let html = "";
-    
+
     for (let i = 0; i < answers.length; i++) {
         html += `<div class="card-body">
-        <button class="btn" id="answer_${i}" onClick="checkQuestion(${i})">${answers[i]}</p>
+        <button class="btn quiz-btn" id="answer_${i}" onClick="checkQuestion(${i})">${answers[i]}</button>
         </div>`;
     }
-    
+
     return html;
 }
 
 
 function checkQuestion(answerId) {
     let answerState = answerValidation(answerId);
-    
+
     if (answerState) {
         setAnswerCorrect(answerId);
+        console.log("richtig");
     } else {
         setAnswerWrong(answerId);
         setTrueAnswerCorrect();
+        console.log("falsch");
     }
     setSelectedAnswer(answerId);
 }
 
 
 function answerValidation(answerId) {
-    return questions[currentQuestion]["Answers"][answerId] == questions[currentQuestion]["CorrectAnswer"];
+    console.log(currentQuestions[currentQuestion]["Answers"][answerId] == currentQuestions[currentQuestion]["CorrectAnswer"]);
+    return currentQuestions[currentQuestion]["Answers"][answerId] == currentQuestions[currentQuestion]["CorrectAnswer"];
 }
 
 
@@ -94,11 +159,12 @@ function setAnswerWrong(answerId) {
 }
 
 
-function setSelectedAnswer(answerId){
-    questions[currentQuestion]["Selected"]=answerId;
+function setSelectedAnswer(answerId) {
+    currentQuestions[currentQuestion]["Selected"] = answerId;
     renderProgress();
     saveItems();
 }
+
 
 function setAnswerCorrect(answerId) {
     document.getElementById(`answer_${answerId}`).classList.add('btn-success');
@@ -107,7 +173,7 @@ function setAnswerCorrect(answerId) {
 
 function setTrueAnswerCorrect() {
     let answerState = false;
-    for (let i = 0; i < questions[currentQuestion]["Answers"].length; i++) {
+    for (let i = 0; i < currentQuestions[currentQuestion]["Answers"].length; i++) {
         answerState = answerValidation([i]);
         if (answerState) {
             setAnswerCorrect(i);
@@ -124,13 +190,13 @@ function renderProgress() {
 
 
 function getProgressInformationHtml() {
-    return `Frage (${currentQuestion + 1} von ${questions.length})`;
+    return `Frage (${currentQuestion + 1} von ${currentQuestions.length})`;
 }
 
 
 function getProgressBarHtml() {
-    if (questions.length > 0) {
-        let progressValue = (getAnsweredQuestionsAmount() / questions.length) * 100;
+    if (currentQuestions.length > 0) {
+        let progressValue = (getAnsweredQuestionsAmount() / currentQuestions.length) * 100;
         html = `<div class="progress">
             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: ${progressValue}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${progressValue}%</div>
         </div>`;
@@ -142,8 +208,8 @@ function getProgressBarHtml() {
 
 
 function getAnsweredQuestionsAmount() {
-    amount = 0;
-    for (const arrayElement of questions) {
+    let amount = 0;
+    for (const arrayElement of currentQuestions) {
         if (arrayElement["Selected"] != null) {
             amount++;
         }
@@ -152,27 +218,60 @@ function getAnsweredQuestionsAmount() {
 }
 
 
-function goToNextQuestion() {
-    if (currentQuestion < (questions.length - 1)) {
-        currentQuestion++;
-        render();
+function checkIfQuizComplete() {
+    if (getAnsweredQuestionsAmount() == currentQuestions.length) {
+        return true;
     } else {
-        if (getAnsweredQuestionsAmount() == questions.length) {
-            completeQuiz();
-        }
+        return false;
     }
 }
 
+function getCorrectAnswersAmount() {
+    let correctAnswers = 0;
+
+    for (const arrayElement of currentQuestions) {
+        if (arrayElement["Selected"] != null) {
+            if (answerValidation(arrayElement["Selected"])) {
+                correctAnswers++;
+            }
+        }
+    }
+
+    return correctAnswers;
+}
+
+
+
+function quizResultSimple() {
+    let correctAnswers = getCorrectAnswersAmount();
+    return `Your Score: ${correctAnswers} / ${currentQuestions.length}`;
+}
+
 function completeQuiz() {
-    document.getElementById("resultCard").style="";
-    document.getElementById("questionCard").style="display: none;";
+    setItemVisible("resultCard");
+    setItemVisible("trophyImg");
+    setItemInvisible("questionCard");
+    document.getElementById("result-text").innerHTML = quizResultSimple();
+}
+
+
+function goToNextQuestion() {
+    if (currentQuestion < (currentQuestions.length - 1)) {
+        currentQuestion++;
+        renderQuiz();
+    } else {
+        if (checkIfQuizComplete()) {
+            completeQuiz();
+        }
+
+    }
 }
 
 function goToPreviousQuestion() {
     if (currentQuestion > 0) {
         currentQuestion--;
     }
-    render();
+    renderQuiz();
 }
 
 function shuffle(array) {
